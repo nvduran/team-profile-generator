b89cfd19e41d17efdc6b518be41bd5514f6d3e0c
Nick Duran <nvduran@gmail.com>
2021-03-10 16:44:36 -0600

    working page template

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const generatePage = require('./lib/page-template');
 const fs = require('fs');
 
 const inquirer = require('inquirer');
@@ -157,9 +157,9 @@ function nextSelection() {
                     console.log('finishing');
                     fs.writeFile('index.html', generatePage('testname', 'testgit'), err => {
                          if (err) throw err;
-                       
+
                          console.log('Portfolio complete! Check out index.html to see the output!');
-                       });
+                    });
                }
           })
 };
@@ -170,22 +170,3 @@ function managerInfo(mgrName, mgrID, mgrEmail, mgrOffice) {
 };
 
 mgrQuestions()
-
-const generatePage = (name, github) => {
-     return `
-     <!DOCTYPE html> 
-     <html lang="en"> 
-     <head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-       <title>Portfolio Demo</title>
-     </head>
-   
-     <body>
-       <h1>${name}</h1>
-       <h2><a href="https://github.com/${github}">Github</a></h2>
-     </body>
-     </html>
-     `;
-};
